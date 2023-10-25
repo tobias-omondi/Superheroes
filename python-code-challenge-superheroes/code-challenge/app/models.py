9ed028b56c3e0ccd4f2cb91efb367be5eb6db0fb
tobias <tobias.omondi@student.moringaschool.com>
2023-10-25 17:35:31 +0300

    updated

--- python-code-challenge-superheroes/code-challenge/app/models.py
+++ python-code-challenge-superheroes/code-challenge/app/models.py
@@ -7,4 +7,24 @@ class Hero(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-# add any models you may need. 
+    # add any models you may need. 
+
+    powers = db.relationship('Power', secondary='hero_powers', back_populates='heroes')
+
+class Power(db.Model):
+    __tablename__ = 'power'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    heroes = db.relationship('Hero', secondary='hero_powers', back_populates='powers')
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), primary_key=True)
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), primary_key=True)
+
+    hero = db.relationship('Hero', back_populates='powers')
+    power = db.relationship('Power', back_populates='heroes')
+
+db.create_all()
